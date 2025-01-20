2d9274e512231efeda3e2f596a5330d6949d4258
codesthenos <codesthenos@gmail.com>
2025-01-20 17:29:30 +0100

    corrijo uuna cosilla del base en el astro config a ver si ahora van bien los paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  base: 'CODESTHENOS-PORTFOLIO'
+  base: '/CODESTHENOS-PORTFOLIO/'
 });
